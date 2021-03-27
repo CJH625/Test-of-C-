4bf27810697d33ee875e8f5627cd34b1fdc28686
CJH625 <77145293+CJH625@users.noreply.github.com>
2021-03-27 17:18:21 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#define £» ;//Don't do this
+
+int main()
+{
+	if (__cplusplus == 201703L)
+		std::cout << "C++17\n";
+	else if (__cplusplus == 201402L)
+		std::cout << "C++14\n";
+	else if (__cplusplus == 201103L)
+		std::cout << "C++11\n";
+	else if (__cplusplus == 199711)
+		std::cout << "C++98\n";
+	else
+		std::cout << "pre-standard C++\n";
+	std::cin.get();
+	return 0;
+}
+
